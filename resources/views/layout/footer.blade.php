802f4373c71bea14a2354166e117aa75854fed14
SanskarJain2111 <jainsa_1@rknec.edu>
2023-11-24 23:43:05 +0530

    history

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<div class="home-sci">
+    <a href=""><i class='bx bxl-facebook'></i></a>
+    <a href=""><i class='bx bxl-twitter'></i></a>
+    <a href=""><i class='bx bxl-linkedin'></i></a>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-</head>
-<body>
-    <div class="home-sci">
-        <a href=""><i class='bx bxl-facebook'></i></a>
-        <a href=""><i class='bx bxl-twitter'></i></a>
-        <a href=""><i class='bx bxl-linkedin'></i></a>
-    </div>
-</body>
-</html>
+</div>
+
